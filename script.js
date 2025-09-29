94eacb34d9687a5bbe06d4e75fda27a2d9f3e6d1
vinay-kumar6533 <vinaykr6533@gmail.com>
2025-09-29 12:35:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let incre = document.querySelector('.incre');
+let decre = document.querySelector('.decre');
+let num = document.querySelector('.num');
+let reset = document.querySelector('.reset');
+let numberInput = document.querySelector('.number');
+let count=0;
+
+incre.addEventListener('click',() => {
+    let inputValue = parseInt(numberInput.value)||1;
+    count+=inputValue;
+    num.innerText = count;
+});
+
+decre.addEventListener('click',() => {
+    let inputValue = parseInt(numberInput.value)||1;
+    count-=inputValue;
+    num.innerText = count;
+});
+
+reset.addEventListener('click',() => {
+    count = 0;
+    num.innerText = count;
+});
+
+
